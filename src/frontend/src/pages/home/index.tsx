cf7eaaaa33a8fbb04fde56995c7a55d7ede4101e
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-11-19 15:24:30 -0300

    Feat: add custom font

--- src/frontend/src/pages/home/index.tsx
+++ src/frontend/src/pages/home/index.tsx
@@ -1,7 +1,20 @@
+import Form from "./components/cardForm";
+
 export default function Home() {
     return (
-      <div>
-        oiiiiiiiiiiiiiiiiiiiiiiiiii
-      </div>
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="w-[50%] p-3 bg-white rounded">
+                <div>
+                    <div className="w-full flex items-center justify-center">
+                        <span className="bold text-2xl">
+                            Add a new card
+                        </span>
+                    </div>
+                    <div>
+                        <Form />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
