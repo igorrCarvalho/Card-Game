3591bbf85bef67d4eb5789e89e8519a4440f6e6f
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-11-19 10:52:52 -0300

    Feat: install tailwind and remove frontend docker container due to wsl Error

---
+++ src/frontend/src/routes.tsx
@@ -0,0 +1,14 @@
+import { Route, Router, Routes } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/home";
+import Game from "./pages/game";
+
+export default function RoutesProvider() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
+  
+  );
+}
