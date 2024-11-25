115ce0df72952a60adcf034b56fc75226aff16e1
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-11-25 04:20:39 -0300

    Feat<frontend>: Create Cards store

---
+++ src/frontend/src/stores/cardStore.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand'
+import { getCardsFromDB, saveCardToDB } from '../api/index';	
+
+type dataType = {
+    name: string;
+    description: string;
+    rarity: string;
+    damageType: string;
+    armorType: string;
+    superCard: boolean;
+    damage: number;
+    hp: number;
+    armor: number;
+    image: string;
+};
+
+type CardStore = {
+    cards: Array<dataType>;
+    saveCard: (data: dataType) => void;
+    getCards: () => Promise<Array<dataType>>;
+};
+
+const useCardStore = create<CardStore>((get, set) => ({
+    cards: [],
+    saveCard: async (data: dataType) => {
+        await saveCardToDB(data);
+    },
+    getCards: async () => {
+        const cards = await getCardsFromDB();
+        useCardStore.setState({ cards });
+        return cards;
+    },
+}))
+
+export default useCardStore;
