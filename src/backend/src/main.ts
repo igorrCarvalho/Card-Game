dfee2fb15c9f78ac5da4c93d95ae5730f3644dec
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-11-25 03:39:33 -0300

    Feat<backend>: Enable CORS

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3001;
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(PORT, () => {
     console.log(
       `Running API in MODE: ${process.env.NODE_ENV} on PORT: ${PORT}`
