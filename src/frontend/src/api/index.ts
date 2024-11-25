027cf5d53989ec12a7edcee9c6c600003040e7f8
igorrCarvalho <igorsilvabhz6@gmail.com>
2024-11-25 03:40:49 -0300

    Fix<frontend>: saveCardToDB types and logic

--- src/frontend/src/api/index.ts
+++ src/frontend/src/api/index.ts
@@ -2,6 +2,20 @@ import axios from "axios";
 
 const defaultUrl = "http://localhost:3001/api/v1/";
 
-export function saveCardToDB({ name, description, rarity, damageType, armorType, superCard, damage, hp, armor, image }) {
-  return axios.post(`${defaultUrl}cards`, { name, description, rarity, damageType, armorType, superCard, damage, hp, armor, image });
+type dataType = {
+    name: string;
+    description: string;
+    rarity: string;
+    damageType: string;
+    armorType: string;
+    superCard: boolean;
+    damage: number;
+    hp: number;
+    armor: number;
+    image: string;
+}
+
+export async function saveCardToDB({ name, description, rarity, damageType, armorType, superCard, damage, hp, armor, image }: dataType) {
+  const response = await axios.post(`${defaultUrl}cards/`, { name, description, rarity, damageType, armorType, superCard, damage, hp, armor, image });
+  return response.data;
 }
